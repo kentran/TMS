8c0320374121fc1399b6e3f95d511763fd9cae99
Ken Tran <gonewiththewind152@gmail.com>
2013-10-29 16:40:41 +0800

    add seed data for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Create universities
-University.create ([
+universities = University.create ([
 	{ name: 'TMS', description: 'TMS administration' },
 	{ name: 'NUS', description: 'A very awesome University. This is where Ken studied' },
 	{ name: 'NTU', description: 'This is another awesome university' }
 ])
 
 # Create departments
-Department.create ([
-	{ name: 'Technical', university: University.find_by_name('TMS') },
-	{ name: 'Electrical and Computer Engineering', university: University.find_by_name('NUS') },
-	{ name: 'Computer Science', university: University.find_by_name('NUS') },
-	{ name: 'Computer Science', university: University.find_by_name('NTU') },
-	{ name: 'Electrical Engineering', university: University.find_by_name('NTU') },
+departments = Department.create ([
+	{ name: 'Technical', university: universities.first },
 ])
 
 # Create admin users
@@ -32,7 +28,7 @@ User.create([
 		last_name: 'Tran',
 		title: 'Mr.',
 		role: 'Admin',
-		university: University.find_by_name('TMS'),
-		university: Department.find_by_name('Technical')
+		university: universities.first,
+		department: departments.first
 	}
 ])
