49615a0ed4966d1730e518b524bca989a0acc46e
Ken Tran <gonewiththewind152@gmail.com>
2013-11-03 22:22:54 +0800

    fix import user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -138,7 +138,7 @@ class UsersController < Devise::RegistrationsController
   #
   def batch_create
     email_list = params[:email_list].split
-    @univeristy = current_user.university
+    @university = current_user.university
     @department = current_user.department
 
     email_list.each do |email|
@@ -147,8 +147,8 @@ class UsersController < Devise::RegistrationsController
       user = User.new(:email => email, :password => generated_password, :role => params[:role])
 
       if user.save
-        university.users << user
-        department.users << user
+        @university.users << user
+        @department.users << user
         record_activity("created new user (batch): " + user.email)
         # Send an email with generated password to the user
         UserMailer.welcome_email(user).deliver
